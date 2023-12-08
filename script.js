0b45338fdaf989779a54dd00d02e7ccbf251f96e
Jeffreydne <Jeffreydne@gmail.com>
2023-12-08 11:23:16 -0800

    add id & comments in HTM, add DOM variables in jsL

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // OMDB apikey=60ccc490
-console.log("Iwork")
+console.log("I work");
+// DOM variables
+const movieDisplayDiv = document.querySelector("#movieDisplay");
+const movieName = document.querySelector("#movie-name");
+const inputPoster = document.querySelector("#poster");
+const inputActors = document.querySelector("#actors");
+const inputPlot = document.querySelector("#plot");
+const inputRating = document.querySelector("#rating");
+const inputYear = document.querySelector("#year");
+const inputRuntime = document.querySelector("#run-time");
+const inputDirector = document.querySelector("#director");
+const inputWriters = document.querySelector("#writers");
+const inputAwards = document.querySelector("#awards");
+
+// other variables
+
+
+
