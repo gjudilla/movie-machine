1e0ea4381c5567f55c4ae183be3471b57fa86ef9
Jeffreydne <Jeffreydne@gmail.com>
2023-12-12 13:12:37 -0800

    add fxn to display searched movies

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("I work");
 // DOM variables
 const movieDisplayDiv = document.querySelector("#movieDisplay");
 // New DOM element
-const previousMovies = document.querySelector("previousSearches");
+const previousMovies = document.querySelector("#previousSearches");
 const movieTitleDOM = document.querySelector("#movie-title");
 const inputPoster = document.querySelector("#poster");
 const inputActors = document.querySelector("#actors");
@@ -23,7 +23,47 @@ let movieNameTestArray = [];
 // other variables
 
 
-//New Function
+//2 New Functions lines 27 to ~82
+// #1
+// function to get all movie names previously selected by user and then display the most recent 10 in the DOM
+let displayPreviousMovies = () => {
+    // When 1st called on loading website there will be no movies in local storage so no change will be made to DOM
+    if(!localStorage.movieArr) {
+        return;
+    } 
+    // if movieArr exists in localStorage 1st add back the h2 for "Previous Searches", then parse the localStorage.movieArr into an array of objects
+
+    const prevMovieH2 = document.createElement('h2');
+    //Add attribute class="space-y-s" to containing div, then, add text content, then append
+    prevMovieH2.setAttribute('class', 'font-semibold text-3xl font-bold mb-4');
+    prevMovieH2.textContent = 'Previous Searches';
+    previousMovies.append(prevMovieH2);
+
+// now parse
+    arrToPost = JSON.parse(localStorage.getItem('movieArr'));   
+    //if length is 10 or more only post the most recent 10 movies saved by using for loop starting at last movie saved
+    if (arrToPost.length > 9) {
+        for(let i = arrToPost.length - 1; i > arrToPost.length - 11; i--) {
+            const newMovieDiv = document.createElement('div');
+            //Add attribute class="space-y-s"
+            newMovieDiv.setAttribute('class', 'space-y-s');
+            //Add actual button using inner HTML
+            newMovieDiv.innerHTML = `<button class="w-full text-left px-4 py-2 text-sm text-white bg-teal-500 rounded-lg hover:bg-teal-700">${arrToPost[i].movieNameStored}</button>`;
+            previousMovies.append(newMovieDiv);
+        }
+    } else {
+        // if there are between 1 and 9 movies in localStorage all movies will be displayed by using for loop starting at last movie saved
+        for(let j = arrToPost.length - 1; j > -1; j--) {
+            const newMovieDiv = document.createElement('div');
+            //Add attribute class="space-y-s" to containing div
+            newMovieDiv.setAttribute('class', 'space-y-s');
+            //Add actual button and its classes/content using inner HTML then append to DOM
+            newMovieDiv.innerHTML = `<button class="w-full text-left px-4 py-2 text-sm text-white bg-teal-500 rounded-lg hover:bg-teal-700">${arrToPost[j].movieNameStored}</button>`;
+            previousMovies.append(newMovieDiv);
+        }
+    }
+}
+// #2
 // function to store movie name, and associated info, in local storage & call new fxn to display last 10 searched movies in DOM. The display fxn to be called each time a new movie is searched for.
 const storeMovieName = (movieToStore, poster, domOptions) => {
     let storageObj = {
@@ -31,6 +71,7 @@ const storeMovieName = (movieToStore, poster, domOptions) => {
         options: `${domOptions}`,
         posterUrl: `${poster}`
     }
+    console.log(storageObj);
     // existingmovieArr will get an array of onjects from local storage, or if empty will give empty array. We can add another movie as only member of the array if we prefer to have a default movie on loading)
     const existingMovieArr = JSON.parse(localStorage.getItem('movieArr')) || [];
     // check to see if movie already exists in localStorage and if so do not store again.
@@ -42,16 +83,17 @@ const storeMovieName = (movieToStore, poster, domOptions) => {
     // if current movie is a new movie add it as an object to current array and set that array as the new localStorage array
     const newMovieArr = [...existingMovieArr, storageObj];
     localStorage.setItem('movieArr', JSON.stringify(newMovieArr));
-    // clear movieDiv of current contents to avoid duplicates, then call displayPreviousMovies() to fill this DIV with all movies in localStorage (most recently stored movie will be last).
-    // THIS DIV NO LONGER EXISTS
-    // movieDiv.innerHTML = "";
+    // clear previousMovies of current contents to avoid duplicates, then call displayPreviousMovies() to fill this DIV with all movies in localStorage (most recently stored movie will be last).
 
+    previousMovies.innerHTML = "";
+
+    // Call fxn to display previous movies in DOM
     displayPreviousMovies();
 }
 // NEW FXN This will be the displayPreviousMovies function
-let displayPreviousMovies = () => {
-    console.log('placeholder for a function');
-}
+// let displayPreviousMovies = () => {
+//     console.log('placeholder for a function');
+// }
 
 function  searchMovies (movieInput) {
 
